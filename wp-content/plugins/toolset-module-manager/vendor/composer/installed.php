cc23b72772fcea56e77ed09b46b836f581495ed8
Lindsay Hoyt <hoyt@sog.unc.edu>
2024-10-18 15:10:42 -0400

    Adding some helpful plugins for future application development.

---
+++ wp-content/plugins/toolset-module-manager/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.8.10',
+        'version' => '1.8.10.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a41a95461cb5a6185ce033706a61a48877566013',
+        'name' => 'toolset/module-manager',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(),
+            'reference' => 'ea4755f8932eb1bd1c1152495811e9edb7cf3301',
+            'dev_requirement' => false,
+        ),
+        'toolset/module-manager' => array(
+            'pretty_version' => '1.8.10',
+            'version' => '1.8.10.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a41a95461cb5a6185ce033706a61a48877566013',
+            'dev_requirement' => false,
+        ),
+    ),
+);
