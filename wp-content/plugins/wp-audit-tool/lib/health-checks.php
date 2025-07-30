5d099d762476a63fb6083e35747468a7511a0ac5
Lindsay Hoyt <hoyt@sog.unc.edu>
2025-07-29 23:13:53 -0400

    Adding some plugins that we use generally use on our sites. mail-queue, post-smtp, wp-all-import-pro, wp-audit-tool, gravityformsmailchimp.

---
+++ wp-content/plugins/wp-audit-tool/lib/health-checks.php
@@ -0,0 +1,19 @@
+<?php
+
+add_filter('debug_information', 'wp_audit_tool_debug_information');
+function wp_audit_tool_debug_information($info) {
+    foreach( glob(__DIR__ . '/health-checks/*.php') as $file ) {
+        require_once $file;
+    }
+
+   $info = WP_Audit_Tool_Health_Check_PHP::results($info);
+   $info = WP_Audit_Tool_Health_Check_Plugins_Known_Issues::results($info);
+   $info = WP_Audit_Tool_Health_Check_Non_Core_DB_Tables::results($info);
+   $info = WP_Audit_Tool_Health_Check_DB_Sizes::results($info);
+   $info = WP_Audit_Tool_Health_Check_WP_Cron::results($info);
+   $info = WP_Audit_Tool_Health_Check_Sys_Cron::results($info);
+   $info = WP_Audit_Tool_Health_Check_Env_Variables::results($info);
+   $info = WP_Audit_Tool_Health_Check_ACF::results($info);
+
+   return $info;
+}
---
+++ wp-content/plugins/wp-audit-tool/lib/health-checks/health-check-php.php
@@ -0,0 +1,46 @@
+<?php
+
+class WP_Audit_Tool_Health_Check_PHP 
+{
+    public static function results($info) {
+
+        $ip = !empty($_SERVER[ 'SERVER_ADDR' ]) ? $_SERVER[ 'SERVER_ADDR' ] : (!empty($_SERVER[ 'LOCAL_ADDR' ]) ? $_SERVER[ 'LOCAL_ADDR' ] : '');
+        $info['wp-server']['fields']['server_ip'] = [
+            'label' => __('Server IP Address', 'wp-audit-tool'),
+            'value' => $ip,
+            'debug' => $ip,
+        ];
+        
+        $info['wp-server']['fields']['php_ini_loaded_file'] = [
+            'label' => __('PHP.ini loaded file', 'wp-audit-tool'),
+            'value' => php_ini_loaded_file(),
+            'debug' => php_ini_loaded_file(),
+        ];
+     
+        $info['wp-server']['fields']['php_ini_scanned_files'] = [
+            'label' => __('PHP.ini scanned files', 'wp-audit-tool'),
+            'value' => php_ini_scanned_files(),
+            'debug' => php_ini_scanned_files(),
+        ];
+        
+        $info['wp-server']['fields']['display_errors'] = [
+            'label' => __('PHP display errors', 'wp-audit-tool'),
+            'value' => ini_get('display_errors') ? __( 'Yes' ) : __( 'No' ),
+            'debug' => ini_get('display_errors'),
+        ];
+     
+        $info['wp-server']['fields']['register_globals'] = [
+            'label' => __('PHP.ini registered globals', 'wp-audit-tool'),
+            'value' => ini_get('register_globals'),
+            'debug' => ini_get('register_globals'),
+        ];
+        
+        $info['wp-server']['fields']['register_globals'] = [
+            'label' => __('PHP loaded extensions', 'wp-audit-tool'),
+            'value' => implode(", ", get_loaded_extensions()),
+            'debug' => get_loaded_extensions(),
+        ];
+     
+        return $info;
+     }
+}
